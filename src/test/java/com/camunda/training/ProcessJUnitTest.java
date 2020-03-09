47faf139e7189749737da5a30c8330a27306a0f3
radim.balusek@seznam.cz <radim.balusek@seznam.cz>
2020-03-09 12:26:38 +0100

    Camunda training - finish task 5
    - switch review tweet to manual task
    - add rename plugin and rename ids
    - candidate group set to management
    - added List<Task> taskList = taskService()
    - added    assertThat(taskList).isNotNull();assertThat(taskList).hasSize(1);
    - remove variable.put("approved", true),
    - update
       Map<String, Object> approvedMap = new HashMap<String, Object>();
       approvedMap.put("approved", true);
       taskService().complete(task.getId(), approvedMap);

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,12 +1,15 @@
 package com.camunda.training;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.assertj.core.api.Assertions;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.engine.task.Task;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -31,11 +34,27 @@ public class ProcessJUnitTest {
   public void testHappyPath() {
     // Create a HashMap to put in variables for the process instance
     Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("approved", true);
-    variables.put("content", "Exercise 4 test - RADIM B");
+   // variables.put("approved", true);
+    variables.put("content", "Exercise 10 test - RADIM B");
     // Start process with Java API and variables
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQA", variables);
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
     // Make assertions on the process instance
-   // assertThat(processInstance).isEnded();  // sucesss just if process ended
+    assertThat(processInstance).isWaitingAt("ReviewTweetTask");  // sucesss just if process ended
+
+    List<Task> taskList = taskService()
+            .createTaskQuery()
+            .taskCandidateGroup("management")
+            .processInstanceId(processInstance.getId())
+            .list();
+
+    Task task = taskList.get(0);
+
+    Map<String, Object> approvedMap = new HashMap<String, Object>();
+    approvedMap.put("approved", true);
+    taskService().complete(task.getId(), approvedMap);
+
+    Assertions.assertThat(taskList).isNotNull();
+    Assertions.assertThat(taskList).hasSize(1);
+
   }
 }
